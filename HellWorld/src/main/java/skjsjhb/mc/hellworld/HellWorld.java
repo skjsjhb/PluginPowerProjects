4c1cc162cc11a2d7d2984731ae5204a8236e56d1
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-12-29 11:11:48 +0800

    init(*): initial commit

---
+++ HellWorld/src/main/java/skjsjhb/mc/hellworld/HellWorld.java
@@ -0,0 +1,15 @@
+package skjsjhb.mc.hellworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HellWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("hell, world");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
