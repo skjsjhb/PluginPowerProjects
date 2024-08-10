2c8333ae1c2bac06f219f10439db36a2351f4ded
skjsjhb <skjsjhb@outlook.com>
2024-08-10 16:40:03 +0800

    Initial commit

---
+++ HellWorld/src/main/java/moe/skjsjhb/mc/hellWorld/HellWorld.java
@@ -0,0 +1,15 @@
+package moe.skjsjhb.mc.hellWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HellWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("hell, world");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
